ff83bfe1c9726fb6ea6c16c5d2b726201add638a
Evgeniya Meshuris <em@theemm.com>
2022-03-25 10:18:34 -0700

    add form to index.html, add submit button, add function to clear the form

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,46 @@
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
+$(document).ready(function () {
+  $("form#suggester").submit(function (event) {
     event.preventDefault();
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
+    const startOverText = 'Start Over';
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    if ($("#btnSubmit").html() == startOverText){
+      resetForm();
+      return;
+    }
 
-    $("#story").show();
+    const name = $("#name").val();
+    const age = parseInt($("#age").val());
+    const ocupation = parseInt($("#ocupation").val());
+    const experiense = parseInt($("#experiense").val());
+    const answer = $("input:radio[name=answer]:checked").val();
+
+    // if (ocupation === 4) {
+    //   return alert("CS");
+    // }
+    // } else if (secondVal + thirdVal <= firstVal) {
+    //   return alert("NOT a triangle");
+    // } else if (firstVal + thirdVal <= secondVal) {
+    //   return alert("NOT a triangle");
+    // } 
+    if (experiense > 10 && ocupation === 4) {
+      $('#brainfuck').show();
+    } else if (age < 18) {
+      $('#python').show();
+    } else if (ocupation === 3 || ocupation === 2) {
+      $('#cs').show();
+    } else if (answer === 'yes' && experiense === 0) {
+      $('#python').show();
+    }
+
+    $("#btnSubmit").html(startOverText);
+    
   });
 });
+
+function resetForm(){
+  $("#age").val('');
+  $('#python').hide();
+  $("#btnSubmit").html('Submit');
+}
+// const result = add(name, number2);
+//     $("#output").text(result);
