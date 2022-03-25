c6afed8450e456e90d8df89a6dfa3954adad2aef
Evgeniya Meshuris <em@theemm.com>
2022-03-25 13:00:05 -0700

    add logic for the result, add validation for name and age field

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-  $("form#suggester").submit(function (event) {
-    //event.preventDefault();
+  $("#suggester").submit(function (event) {
+    event.preventDefault();
     const startOverText = 'Start Over';
 
     if ($("#btnSubmit").html() == startOverText) {
@@ -8,24 +8,26 @@ $(document).ready(function () {
       return;
     }
 
-    $("form#suggester").validate();
+    $("#suggester").validate();
 
     const name = $("#name").val();
     const age = parseInt($("#age").val());
     const occupation = parseInt($("#occupation").val());
     const experience = parseInt($("#experience").val());
-    const answer = $("input:radio[name=answer]:checked").val();
+    const answer = $("input:radio[name=finace]:checked").val();
 
     if (experience > 10 && occupation === 4) {
-      $('#brainfuck').show();
+      showResult(name, 'Brainfuck');
     } else if (age > 5 && age < 10 && occupation === 0) {
-      $('#kids').show();
-    } else if (occupation === 3 || occupation === 2) {
-      $('#cs').show();
-    } else if (answer === 'yes' && experience === 0) {
-      $('#python').show();
+      showResult(name, 'Codding for Kids');
+    } else if (occupation === 3 || occupation === 2 || occupation === 6) {
+      showResult(name, 'C#');
+    } else if (finace === 'yes' && experience === 0) {
+      showResult(name, 'Python');
     } else if (experience === 0 && age > 21) {
-      $('#epicodus').show();
+      showResult(name, 'Epicodus');
+    } else {
+      showResult(name, 'Epicodus');
     }
 
     $("#btnSubmit").html(startOverText);
@@ -33,45 +35,19 @@ $(document).ready(function () {
   });
 });
 
+function showResult(name, language) {
+  $("#spanResName").text(name);
+  $("#spanResLang").text(language);
+  $('#divResult').show();
+}
+
 function resetForm() {
   $("#name").val('');
   $("#age").val('');
   $("#occupation").val('')
   $("#experience").val('')
   $("input:radio[name=answer]:checked").val('');
-  $("#python").hide();
-  $("#cs").hide();
-  $("#brainfuck").hide();
-  $("#kids").hide();
-  $("#epicodus").hide();
+  $("#divResult").hide();
   $("#btnSubmit").html('Submit');
 }
 
-$(function() {
-  $("form#suggester").validate({
-    rules: {
-      name: "required",
-      age: "required",
-      occupation: "required",
-      experience: "required",
-      finace: "required",
-      age: {
-        required: true,
-        maxlength: 2
-      },
-      experience: {
-        required: /^[0-9]+$/,
-      }
-    },
-    messages: {
-      name: "Please enter your firstname",
-      age: {
-        required: "Please enter your age",
-        maxlength: "Don't you want to have a rest insted of learning programming language"
-      },
-    },
-    submitHandler: function(form) {
-      form.submit();
-    }
-  });
-});
