361ec8c18c3d777677dd10748708d23a4ed27f2b
Evgeniya Meshuris <em@theemm.com>
2022-03-27 10:29:47 -0700

    fix field validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,38 @@
-const startOverText = 'Start Over';
+/*global $*/
+const startOverText = "Start Over";
 $(document).ready(function () {
   $("#suggesterForm").submit(function (event) {
     event.preventDefault();
-    
 
     if ($("#btnSubmit").html() == startOverText) {
       resetForm();
       return;
     }
 
-    $("#suggesterForm").validate();
-
     const name = $("#name").val();
     const age = parseInt($("#age").val());
     const occupation = parseInt($("#occupation").val());
     const experience = parseInt($("#experience").val());
     let answer = $("input:radio[name=answer]:checked").val();
-    let kids_ans = (age >= 5 && age < 13)
+    let kids_ans = age >= 5 && age < 13;
 
     if (answer === "no") {
-      showResult(name, 'Biology');
+      showResult(name, "Biology");
     }
 
     if (answer === "yes" && kids_ans) {
-      showResult(name, 'Codding for Kids');
-    } else if (age >= 13 && age <21) {
-      showResult(name, 'Python');
+      showResult(name, "Codding for Kids");
+    } else if (age >= 13 && age < 21) {
+      showResult(name, "Python");
     } else if (experience > 10 && occupation === 4) {
-      showResult(name, 'Brainfuck');
+      showResult(name, "Brainfuck");
     } else if (occupation >= 1 && age >= 18) {
-      showResult(name, 'C#');
+      showResult(name, "C#");
     } else if (age >= 18 && occupation === 0) {
-      showResult(name, 'HTML and CSS');
+      showResult(name, "HTML and CSS");
     } else {
-      showResult(name, 'Biology');
+      showResult(name, "Biology");
     }
-
   });
 });
 
@@ -46,17 +43,16 @@ function showResult(name, language) {
 
   $("#spanResName").text(name);
   $("#spanResLang").text(language);
-  $('#divResult').show();
+  $("#divResult").show();
   $("#btnSubmit").html(startOverText);
 }
 
 function resetForm() {
-  $("#name").val('');
-  $("#age").val('');
-  $("#occupation").val('')
-  $("#experience").val('')
-  $("input:radio[name=answer]:checked").val('');
+  $("#name").val("");
+  $("#age").val("");
+  $("#occupation").val("");
+  $("#experience").val("");
+  $("input:radio[name=answer]:checked").val("");
   $("#divResult").hide();
-  $("#btnSubmit").html('Submit');
+  $("#btnSubmit").html("Submit");
 }
-
